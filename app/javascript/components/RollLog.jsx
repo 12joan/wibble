f2be7d1eba494875a1bb0193763e658f3e87ec89
Joe Anderson <joe@osbert.me>
2021-05-07 08:48:48 +0100

    Scroll to most recent when new roll arrives

--- app/javascript/components/RollLog.jsx
+++ app/javascript/components/RollLog.jsx
@@ -1,75 +1,90 @@
 import React from 'react'
 import RollPart from 'components/RollPart'
 
-const RollLog = props => {
-  let previousName = undefined
+class RollLog extends React.Component {
+  constructor(props) {
+    super(props)
 
-  return (
-    <div className="flex-grow-1 bg-light p-3 overflow-scroll d-flex flex-column-reverse" aria-label="Roll log">
-      <div>
-        <div className="visually-hidden-focusable" id="top-of-roll-log">
-          This is the top of the roll log. <a href="#bottom-of-roll-log">Skip to most recent</a>
-        </div>
+    this.scrollEl = React.createRef()
+  }
 
-        <div className="text-center text-secondary mt-3">
-          <h3>Thanks for joining</h3>
-          <p className="lead">Room ID <span className="font-monospace">{props.roomId}</span></p>
-        </div>
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.rollData.length > prevProps.rollData.length) {
+      // Scroll to most recent
+      this.scrollEl.current.scrollTo(0, 0)
+    }
+  }
 
-        {
-          props.rollData.map((data, i) => {
-            const { name } = data.user
+  render() {
+    let previousName = undefined
 
-            const showName = name !== previousName
+    return (
+      <div ref={this.scrollEl} className="flex-grow-1 bg-light p-3 overflow-scroll d-flex flex-column-reverse" aria-label="Roll log">
+        <div>
+          <div className="visually-hidden-focusable" id="top-of-roll-log">
+            This is the top of the roll log. <a href="#bottom-of-roll-log">Skip to most recent</a>
+          </div>
 
-            previousName = name
+          <div className="text-center text-secondary mt-3">
+            <h3>Thanks for joining</h3>
+            <p className="lead">Room ID <span className="font-monospace">{this.props.roomId}</span></p>
+          </div>
 
-            return (
-              <div key={i}>
-                {
-                  showName && (
-                    <h2 className="text-secondary fs-5 fw-normal mt-4">{name}</h2>
-                  )
-                }
+          {
+            this.props.rollData.map((data, i) => {
+              const { name } = data.user
 
-                <div className="card card-body mt-2" aria-label={ `Dice roll ${data.roll.name || ''} ${data.roll.result.value}` } tabIndex="0">
+              const showName = name !== previousName
+
+              previousName = name
+
+              return (
+                <div key={i}>
                   {
-                    data.roll.name !== null && (
-                      <h4 className="mb-2">{`${data.roll.name} (${data.roll.result.value})`}</h4>
+                    showName && (
+                      <h2 className="text-secondary fs-5 fw-normal mt-4">{name}</h2>
                     )
                   }
 
-                  <div className="d-flex justify-content-between my-2" aria-label={`Individual die images. Roll result is ${data.roll.result.value}`}>
-                    <div className="d-flex flex-wrap align-items-center">
+                  <div className="card card-body mt-2" aria-label={ `Dice roll ${data.roll.name || ''} ${data.roll.result.value}` } tabIndex="0">
+                    {
+                      data.roll.name !== null && (
+                        <h4 className="mb-2">{`${data.roll.name} (${data.roll.result.value})`}</h4>
+                      )
+                    }
+
+                    <div className="d-flex justify-content-between my-2" aria-label={`Individual die images. Roll result is ${data.roll.result.value}`}>
+                      <div className="d-flex flex-wrap align-items-center">
+                        {
+                          data.roll.result.parts.map((part, i) => (
+                            <RollPart key={i} part={part} />
+                          ))
+                        }
+                      </div>
+
                       {
-                        data.roll.result.parts.map((part, i) => (
-                          <RollPart key={i} part={part} />
-                        ))
+                        data.roll.name === null && (
+                          <span className="fs-2 text-nowrap" aria-label={ `Roll result ${data.roll.result.value}` }>({data.roll.result.value})</span>
+                        )
                       }
                     </div>
 
-                    {
-                      data.roll.name === null && (
-                        <span className="fs-2 text-nowrap" aria-label={ `Roll result ${data.roll.result.value}` }>({data.roll.result.value})</span>
-                      )
-                    }
-                  </div>
-
-                  <div className="text-muted">
-                    {data.roll.result.text}
+                    <div className="text-muted">
+                      {data.roll.result.text}
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
 
-        <div className="visually-hidden-focusable" id="bottom-of-roll-log">
-          This is the bottom of the roll log. <a href="#top-of-roll-log">Skip to oldest</a>
+          <div className="visually-hidden-focusable" id="bottom-of-roll-log">
+            This is the bottom of the roll log. <a href="#top-of-roll-log">Skip to oldest</a>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default RollLog
