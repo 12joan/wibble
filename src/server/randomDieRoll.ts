6ff26b595ae1764869a9e4fce9f62d06e9f34a6a
Joe Anderson <joe@mousetrapped.co.uk>
2023-06-11 20:45:49 +0100

    Add random number generator

--- src/server/randomDieRoll.ts
+++ src/server/randomDieRoll.ts
@@ -1,3 +1,11 @@
-export const randomDieRoll = async () => {
-  return 1;
-};
+import { randomInt } from 'crypto';
+
+export const randomDieRoll = (sides: number) => new Promise<number>((resolve, reject) => {
+  randomInt(1, sides + 1, (err, n) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(n);
+    }
+  });
+});
