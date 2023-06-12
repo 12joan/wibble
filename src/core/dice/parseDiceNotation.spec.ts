5e9d7c3e7a8e883b6bd8136b09c4ad5c69375451
Joe Anderson <joe@mousetrapped.co.uk>
2023-06-12 11:49:41 +0100

    Implement parsing logic

---
+++ src/core/dice/parseDiceNotation.spec.ts
@@ -0,0 +1,107 @@
+import { parseDiceNotation } from './parseDiceNotation';
+
+describe('parseDiceNotation', () => {
+  it('should parse d6', () => {
+    expect(parseDiceNotation('d6')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+    ]);
+  });
+
+  it('should parse 1d6', () => {
+    expect(parseDiceNotation('1d6')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+    ]);
+  });
+
+  it('should parse 2d6', () => {
+    expect(parseDiceNotation('2d6')).toEqual([
+      { type: 'dice', die: 6, count: 2 },
+    ]);
+  });
+
+  it('should parse 1d6+1 (without spaces)', () => {
+    expect(parseDiceNotation('1d6+1')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'modifier', value: 1 },
+    ]);
+  });
+
+  it('should parse 1d6 + 1 (with spaces)', () => {
+    expect(parseDiceNotation('1d6 + 1')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'modifier', value: 1 },
+    ]);
+  });
+
+  it('should parse 1d6-1 (without spaces)', () => {
+    expect(parseDiceNotation('1d6-1')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'modifier', value: -1 },
+    ]);
+  });
+
+  it('should parse 1d6 - 1 (with spaces)', () => {
+    expect(parseDiceNotation('1d6 - 1')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'modifier', value: -1 },
+    ]);
+  });
+
+  it('should parse 1d6+-1', () => {
+    expect(parseDiceNotation('1d6+-1')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'modifier', value: -1 },
+    ]);
+  });
+
+  it('should parse 1', () => {
+    expect(parseDiceNotation('1')).toEqual([{ type: 'modifier', value: 1 }]);
+  });
+
+  it('should parse -1', () => {
+    expect(parseDiceNotation('-1')).toEqual([{ type: 'modifier', value: -1 }]);
+  });
+
+  it('should parse 1d6+1d4', () => {
+    expect(parseDiceNotation('1d6+1d4')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'dice', die: 4, count: 1 },
+    ]);
+  });
+
+  it('should parse 1d6+ 1d4+10', () => {
+    expect(parseDiceNotation('1d6+ 1d4+10')).toEqual([
+      { type: 'dice', die: 6, count: 1 },
+      { type: 'dice', die: 4, count: 1 },
+      { type: 'modifier', value: 10 },
+    ]);
+  });
+
+  it('should parse 1d20A', () => {
+    expect(parseDiceNotation('1d20A')).toEqual([
+      { type: 'dice', die: '20A', count: 1 },
+    ]);
+  });
+
+  it('should parse 1d20D', () => {
+    expect(parseDiceNotation('1d20D')).toEqual([
+      { type: 'dice', die: '20D', count: 1 },
+    ]);
+  });
+
+  it('should parse 1d20a', () => {
+    expect(parseDiceNotation('1d20a')).toEqual([
+      { type: 'dice', die: '20A', count: 1 },
+    ]);
+  });
+
+  it('should parse 1d20d', () => {
+    expect(parseDiceNotation('1d20d')).toEqual([
+      { type: 'dice', die: '20D', count: 1 },
+    ]);
+  });
+
+  it('should return null for invalid notation', () => {
+    expect(parseDiceNotation('x')).toBeNull();
+  });
+});
---
+++ src/core/dice/parseDiceNotation.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-cond-assign */
+import { TDiceRollRequestPart } from './types';
+
+export const parseDiceNotation = (
+  notation: string
+): TDiceRollRequestPart[] | null => {
+  const lexemes = notation
+    .replace(/\s+/g, '')
+    .split(/([+-])/)
+    .filter((lexeme) => lexeme !== '');
+
+  let sign: 1 | -1 = 1;
+  const parts: TDiceRollRequestPart[] = [];
+  let diceMatch: RegExpMatchArray | null;
+  let modifierMatch: RegExpMatchArray | null;
+  let error = false;
+
+  lexemes.forEach((lexeme) => {
+    if (lexeme === '+') {
+      // Do nothing
+    } else if (lexeme === '-') {
+      sign *= -1;
+    } else if ((diceMatch = lexeme.match(/^(\d*)d(\d+|20[adAD])$/))) {
+      const [, countString, dieString] = diceMatch;
+      const count = countString ? parseInt(countString, 10) : 1;
+
+      const die = /[^0-9]/.test(dieString)
+        ? (dieString.toUpperCase() as '20A' | '20D')
+        : parseInt(dieString, 10);
+
+      parts.push({ type: 'dice', count, die });
+    } else if ((modifierMatch = lexeme.match(/^(\d+)$/))) {
+      const [, valueString] = modifierMatch;
+      const value = sign * parseInt(valueString, 10);
+      parts.push({ type: 'modifier', value });
+      sign = 1;
+    } else {
+      error = true;
+    }
+  });
+
+  return error ? null : parts;
+};
