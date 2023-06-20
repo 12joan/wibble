d04c2a9d54c0d5a8367bc221c6fff83df8d16b16
Joe Anderson <joe@mousetrapped.co.uk>
2023-06-20 09:45:59 +0100

    Implement select menu

--- src/client/CharacterSheet.tsx
+++ src/client/CharacterSheet.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import * as Icons from 'react-bootstrap-icons';
 import { TPostingAs } from '../core/types';
+import { Select } from './Select';
 
 export interface CharacterSheetProps {
   postingAs: TPostingAs;
@@ -14,8 +16,58 @@ export const CharacterSheet = ({
   const setId = (id: string) => setPostingAs((prev) => ({ ...prev, id }));
   const setName = (name: string) => setPostingAs((prev) => ({ ...prev, name }));
 
+  const items = ['one', 'two', 'three', 'four', 'five'];
+  const [selectedItem, setSelectedItem] = React.useState(items[0]);
+
   return (
     <div className="space-y-4">
+      <Select.Root value={selectedItem} onValueChange={setSelectedItem}>
+        <Select.Trigger className="max-md:max-w-none">
+          <Select.Value>
+            <div className="flex items-center gap-2">
+              <Icons.PersonCircle aria-hidden />
+              {selectedItem}
+            </div>
+          </Select.Value>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content>
+            <Select.Viewport>
+              <Select.Item value="x">
+                <Select.ItemText>
+                  <div className="flex items-center gap-2">
+                    <Icons.PersonLinesFill aria-hidden />
+                    Manage profiles
+                  </div>
+                </Select.ItemText>
+              </Select.Item>
+
+              <Select.Item value="y">
+                <Select.ItemText>
+                  <div className="flex items-center gap-2">
+                    <Icons.PersonPlusFill aria-hidden />
+                    New profile
+                  </div>
+                </Select.ItemText>
+              </Select.Item>
+
+              <Select.Separator />
+              {items.map((item) => (
+                <Select.Item key={item} value={item}>
+                  <Select.ItemText>
+                    <div className="flex items-center gap-2">
+                      <Icons.PersonCircle aria-hidden />
+                      {item}
+                    </div>
+                  </Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+
       <label className="block">
         <div className="font-medium">ID</div>
         <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
---
+++ src/client/Select.tsx
@@ -0,0 +1,113 @@
+import React, { forwardRef } from 'react';
+import * as Icons from 'react-bootstrap-icons';
+import * as UpstreamSelect from '@radix-ui/react-select';
+import { twMerge } from 'tailwind-merge';
+import { KeepOnScreen } from './KeepOnScreen';
+
+const SelectTrigger: typeof UpstreamSelect.SelectTrigger = forwardRef(
+  ({ className, children, ...props }, ref) => {
+    const resolvedClassName = twMerge(
+      'px-3 py-2 border rounded-lg bg-foreground hocus:bg-foreground-dimmed-1 max-w-[16rem] w-full flex justify-between items-center',
+      className
+    );
+
+    return (
+      <UpstreamSelect.SelectTrigger
+        ref={ref}
+        className={resolvedClassName}
+        {...props}
+      >
+        {children}
+
+        <UpstreamSelect.SelectIcon>
+          <Icons.ChevronDown />
+        </UpstreamSelect.SelectIcon>
+      </UpstreamSelect.SelectTrigger>
+    );
+  }
+);
+
+const SelectContent: typeof UpstreamSelect.SelectContent = forwardRef(
+  ({ className, children, ...props }, ref) => {
+    const resolvedClassName = twMerge(
+      'border rounded-lg bg-foreground w-[--radix-select-trigger-width] no-focus-ring',
+      className
+    );
+
+    return (
+      <UpstreamSelect.SelectContent
+        ref={ref}
+        className={resolvedClassName}
+        position="popper"
+        avoidCollisions={false}
+        sideOffset={-1}
+        {...props}
+      >
+        <KeepOnScreen nearSide="top" className="p-2">
+          {children}
+        </KeepOnScreen>
+      </UpstreamSelect.SelectContent>
+    );
+  }
+);
+
+const SelectViewport: typeof UpstreamSelect.SelectViewport = forwardRef(
+  ({ className, ...props }, ref) => {
+    const resolvedClassName = twMerge('', className);
+
+    return (
+      <UpstreamSelect.SelectViewport
+        ref={ref}
+        className={resolvedClassName}
+        style={{ overflow: undefined }}
+        {...props}
+      />
+    );
+  }
+);
+
+const SelectItem: typeof UpstreamSelect.SelectItem = forwardRef(
+  ({ className, children, ...props }, ref) => {
+    const resolvedClassName = twMerge(
+      'px-3 py-2 rounded-lg hocus:bg-foreground-dimmed-1 flex items-center cursor-pointer no-focus-ring select-none',
+      className
+    );
+
+    return (
+      <UpstreamSelect.SelectItem
+        ref={ref}
+        className={resolvedClassName}
+        {...props}
+      >
+        {children}
+
+        <UpstreamSelect.ItemIndicator className="ml-auto">
+          <Icons.Check2 />
+        </UpstreamSelect.ItemIndicator>
+      </UpstreamSelect.SelectItem>
+    );
+  }
+);
+
+const SelectSeparator: typeof UpstreamSelect.SelectSeparator = forwardRef(
+  ({ className, ...props }, ref) => {
+    const resolvedClassName = twMerge('border-t -mx-2 my-2', className);
+
+    return (
+      <UpstreamSelect.SelectSeparator
+        ref={ref}
+        className={resolvedClassName}
+        {...props}
+      />
+    );
+  }
+);
+
+export const Select = {
+  ...UpstreamSelect,
+  Trigger: SelectTrigger,
+  Content: SelectContent,
+  Viewport: SelectViewport,
+  Item: SelectItem,
+  Separator: SelectSeparator,
+};
